2b2399a07732295635571bc38f321a7510174b7d
Nimbu <harryjohnlancaster@gmail.com>
2026-01-06 16:40:00 +0000

    Initial commit

---
+++ src/main/java/net/nimbu/thaumaturgy/Thaumaturgy.java
@@ -0,0 +1,17 @@
+package net.nimbu.thaumaturgy;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Thaumaturgy implements ModInitializer {
+
+	public static final String MOD_ID = "thaumaturgy";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/nimbu/thaumaturgy/ThaumaturgyClient.java
@@ -0,0 +1,10 @@
+package net.nimbu.thaumaturgy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThaumaturgyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
