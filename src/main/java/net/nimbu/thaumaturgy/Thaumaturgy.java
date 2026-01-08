6468ef4f4b87eaa6cbbfb9daea86ff13ac67e48b
BenjaminPhillips-447 <benphillips390@gmail.com>
2026-01-08 23:54:21 +0000

    Custom dimension finished

--- src/main/java/net/nimbu/thaumaturgy/Thaumaturgy.java
+++ src/main/java/net/nimbu/thaumaturgy/Thaumaturgy.java
@@ -2,6 +2,9 @@ package net.nimbu.thaumaturgy;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.Identifier;
 import net.nimbu.thaumaturgy.block.ModBlocks;
 import net.nimbu.thaumaturgy.component.ModDataComponentTypes;
 import net.nimbu.thaumaturgy.item.ModItemGroups;
@@ -21,5 +24,11 @@ public class Thaumaturgy implements ModInitializer {
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 		ModDataComponentTypes.registerDataComponentTypes();
+		CustomPortalBuilder.beginPortal()
+				.frameBlock(Blocks.AMETHYST_BLOCK)
+				.lightWithItem(ModItems.WAND)
+				.destDimID(Identifier.tryParse(Thaumaturgy.MOD_ID, "pocket_dim"))
+				.tintColor(128,128,128)
+				.registerPortal();
 	}
 }
