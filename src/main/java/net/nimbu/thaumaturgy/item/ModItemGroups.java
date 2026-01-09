d453d74cfb0c7200a852b013b0381910ed0a42a6
Nimbu <harryjohnlancaster@gmail.com>
2026-01-09 00:15:33 +0000

    idk bro we touched tips or something

--- src/main/java/net/nimbu/thaumaturgy/item/ModItemGroups.java
+++ src/main/java/net/nimbu/thaumaturgy/item/ModItemGroups.java
@@ -1,10 +1,32 @@
 package net.nimbu.thaumaturgy.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.nimbu.thaumaturgy.Thaumaturgy;
+
+import net.nimbu.thaumaturgy.block.ModBlocks;
 
 public class ModItemGroups {
 
-    public static void registerItemGroups(){
-        Thaumaturgy.LOGGER.info("Registering item groups for "+Thaumaturgy.MOD_ID);
+    public static final ItemGroup THAUMATURGY_ITEM_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(Thaumaturgy.MOD_ID, "thaumaturgy_items"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.WAND))
+                    .displayName(Text.translatable("itemgroup.thaumaturgy.thaumaturgy_items"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModItems.WAND);
+                        entries.add(ModItems.STAFF);
+                        entries.add(ModBlocks.REVISUALISING_TABLE);
+                        entries.add(ModBlocks.PITCH_BLACK_BLOCK);
+                    }).build());
+
+
+    public static void registerItemGroups() {
+
+        Thaumaturgy.LOGGER.info("Registering Item Groups for " + Thaumaturgy.MOD_ID);
     }
 }
